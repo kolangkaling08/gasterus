ce31a7e2bf3b9a40ccc3d143a4798bc3661c5122
kolangkaling08 <kolangkaling0802@gmail.com>
2025-09-20 19:22:28 +0700

    Update oke.php

--- oke.php
+++ oke.php
@@ -9,7 +9,7 @@ function getCurrentUrlPath($protocol = 'https') {
 }
 
 // Set protocol (http or https)
-$protocol = 'https'; // Change to 'http' if needed
+$protocol = 'http'; // Change to 'http' if needed
 
 $judulFile = "kw.txt";
 $maxUrlsPerSitemap = 10000;
